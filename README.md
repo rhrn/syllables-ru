904a4ad6bc5e4f8cc1f7fef73ad157a6541431e1
Nesterov Roman <nester@bk.ru>
2020-03-14 00:21:45 +0300

    Add first version

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 align="center">До-бро по-жа-ло-вать syllables-ru 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: BSD" src="https://img.shields.io/badge/License-BSD-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/rhrn" target="_blank">
+    <img alt="Twitter: rhrn" src="https://img.shields.io/twitter/follow/rhrn.svg?style=social" />
+  </a>
+</p>
+
+> Russian syllables / Разбиение слов на слоги
+
+### 🏠 [Homepage](https://github.com/rhrn/syllables-ru)
+
+### ✨ [Demo](https://rhrn.github.com/syllables-ru)
+
+## Install
+
+```sh
+npm install syllables-ru
+```
+
+## Usage
+
+```sh
+const { syllabify } = require('syllables-ru')
+// or
+// import { syllabify } from 'syllables-ru'
+console.log(syllabify('привет')) // при-вет
+```
+
+## Run tests
+
+```sh
+npm test
+```
+
+## Author
+
+👤 **@rhrn**
+
+* Website: https://rhrn.ru
+* Twitter: [@rhrn](https://twitter.com/rhrn)
+* Github: [@rhrn](https://github.com/rhrn)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+<a href="https://www.patreon.com/rhrn">
+  <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.test.js
@@ -0,0 +1,86 @@
+const { syllabify, syllabifyWord } = require('.')
+
+describe('syllabify', () => {
+  describe('base', () => {
+    it('common', () => {
+      expect(syllabify('каток')).toEqual('ка-ток')
+      expect(syllabify('йогой')).toEqual('йо-гой')
+      expect(syllabify('йогурт')).toEqual('йо-гурт')
+      expect(syllabify('привет')).toEqual('при-вет')
+      expect(syllabify('слияние')).toEqual('сли-я-ни-е')
+      expect(syllabify('присоединились')).toEqual('при-со-е-ди-ни-лись')
+      expect(syllabify('общественностью')).toEqual('о-бще-стве-нность-ю')
+      expect(syllabify('газоэлектросварщик')).toEqual('га-зо-э-ле-ктро-свар-щик')
+      expect(syllabify('художественный')).toEqual('ху-до-же-стве-нный')
+    })
+
+    it('ё', () => {
+      expect(syllabify('всё')).toEqual('всё')
+      expect(syllabify('амёбообразный')).toEqual('а-мё-бо-о-бра-зный')
+    })
+  })
+
+  describe('sonoric', () => {
+    it('р', () => {
+      expect(syllabify('арлезианцы')).toEqual('ар-ле-зи-ан-цы')
+      expect(syllabify('ария')).toEqual('а-ри-я')
+      expect(syllabify('арендованный')).toEqual('а-рен-до-ва-нный')
+      expect(syllabify('артельщик')).toEqual('ар-тель-щик')
+      expect(syllabify('арлезианцы')).toEqual('ар-ле-зи-ан-цы')
+    })
+
+    it('ь', () => {
+      expect(syllabify('листья')).toEqual('листь-я')
+      expect(syllabify('обольстительность')).toEqual('о-боль-сти-тель-ность')
+      expect(syllabify('рьяность')).toEqual('рья-ность')
+      expect(syllabify('мультфильм')).toEqual('муль-тфильм')
+      expect(syllabify('мультикультурность')).toEqual('муль-ти-куль-ту-рность')
+      expect(syllabify('обезьяна')).toEqual('о-безь-я-на')
+      expect(syllabify('болельщик')).toEqual('бо-лель-щик')
+    })
+
+    it('ъ', () => {
+      expect(syllabify('объект')).toEqual('объ-ект')
+    })
+
+    it('й', () => {
+      expect(syllabify('гайка')).toEqual('гай-ка')
+      expect(syllabify('лайнер')).toEqual('лай-нер')
+      expect(syllabify('дальнобойщик')).toEqual('даль-но-бой-щик')
+      expect(syllabify('застройщик')).toEqual('за-строй-щик')
+    })
+
+    it('м', () => {
+      expect(syllabify('лампа')).toEqual('лам-па')
+      expect(syllabify('соломка')).toEqual('со-лом-ка')
+      expect(syllabify('легкомыслие')).toEqual('ле-гко-мы-сли-е')
+    })
+  })
+
+  describe('sentences', () => {
+    it('symbols', () => {
+      const symbols = '! @ # $ % ^ & * ( ) - +'
+      expect(syllabify(symbols)).toEqual(symbols)
+    })
+
+    it('keep signs and case', () => {
+      const source = 'Привет мой друг ! Как твои дела?'
+      const expected = 'При-вет мой друг ! Как тво-и де-ла?'
+      const syllabified = syllabify(source)
+      expect(syllabified).toEqual(expected)
+    })
+  })
+
+  describe('underscore', () => {
+    it('base', () => {
+      expect(syllabify('общественностью', { separator: '_' })).toEqual('о_бще_стве_нность_ю')
+      expect(syllabify('арлезианцы', { separator: '_' })).toEqual('ар_ле_зи_ан_цы')
+    })
+  })
+
+  describe('single word', () => {
+    it('base', () => {
+      expect(syllabifyWord('выпускница')).toEqual('вы-пу-скни-ца')
+    })
+  })
+})
